8310d00823b8b9ee0472513333f90d9211927446
朝杰 张 <zhangchaojie@bytedance.com>
2021-05-25 11:44:51 +0800

    0.0.1

---
+++ macker.rb
@@ -0,0 +1,15 @@
+class Macker < Formula
+  desc "macker"
+  homepage "https://github.com/assassinor/macker"
+  url "https://github.com/assassinor/macker/releases/download/v0.0.4/macker.zip"
+  sha256 "2c949e0859c1d303292c1bdd54fde75a960faaac8a5bfe71042db7e48f6b2697"
+  license ""
+
+  def install
+    bin.install "macker"
+  end
+
+  test do
+    system "false"
+  end
+end

15f51be44d4a5ccfb30ac56bb071ae88fab9804b
张朝杰 <zhangchaojie@bytedance.com>
2021-12-14 00:02:23 +0800

    commit

--- Formula/macker.rb
+++ Formula/macker.rb
@@ -1,7 +1,7 @@
 class Macker < Formula
   desc "macker"
-  homepage "https://github.com/assassinor/macker"
-  url "https://github.com/assassinor/macker/releases/download/v0.0.4/macker.zip"
+  homepage "https://github.com/macker-m3s/macker"
+  url "https://github.com/macker-m3s/macker/releases/download/v0.0.4/macker.zip"
   sha256 "2c949e0859c1d303292c1bdd54fde75a960faaac8a5bfe71042db7e48f6b2697"
   license ""
 
